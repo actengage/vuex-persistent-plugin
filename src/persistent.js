d4029ef9ab6ba3ad4ca5967e86f5f2264290f130
Justin Kimbrell <jkimbrell@actengage.com>
2021-11-15 11:29:35 -0700

    Updated persistent plugin
    - Save each state key as its own config document
    - Diff the changed state and previous state, and save the key/values that actually changed.

--- src/persistent.js
+++ src/persistent.js
@@ -1,39 +1,47 @@
+import { diff } from 'deep-object-diff';
 import debounce from 'lodash.debounce';
-import { get, init, save } from './Storage';
+import { init } from './Storage';
 
 export default (options = {}) => {
-    const key = options.key || 'vuex';
+    // Initialize the database.
+    const db = init(options.database || 'vuex');
 
-    const db = init(options.database || 'config');
-
+    // Create the debouncer so we can throttle the changes to the db.
     const debounced = debounce((fn, ...args) => {
         fn(...args);
-    }, options.wait || 250);
+    }, options.wait || 175);
 
+    // Define the Vuex plugin with an async function so we can await promises.
     return async(vuex) => {
+        // Compact the database to reduce the size and remove some unnecessary
+        // revisions.
         await db.compact();
     
-        try {
-            Object.assign(vuex.state, await get(key));
-        }
-        catch(e) {
-            //
-        }
+        // Loop through the vuex.state keys and get the values from the db.
+        for(const key of Object.keys(vuex.state)) {
+            vuex.state[key] = await db.config(key);
+        }                
 
-        /**
-         * @todo Save each state key as its own config document.
-         * @todo Diff the changed state and previous state, and save the
-         *       key/values that changed.
-         */
-        vuex.watch(state => state, async(state) => {
-            const doc = Object.keys(state).reduce((carry, key) => {
-                return Object.assign(carry, {
-                    [key]: state[key]
-                });
-            }, {});
-            
-            debounced(() => save(key, doc));
-        }, {
+        // Define the previous state by converting it to a plain object.
+        // This allows us to deef diff the objects later and only save the
+        // changes that we need, instead of saving the entire state every time.
+        let prevState = JSON.parse(JSON.stringify(vuex.state));
+    
+        // Create the watcher using the debouncer callback.
+        vuex.watch(state => state, async(state) => debounced(async() => {
+            // Calculate the differences between the prev/current states
+            const difference = diff(prevState, state);
+
+            // Loop through the differences and save the key/value in the db.
+            for(const [key, value] of Object.entries(difference)) {
+                await db.config(key, value);
+            }                
+
+            // Set the previous back to a plain object so we can compare again
+            // the next time the debouncer callback is fired.
+            prevState = JSON.parse(JSON.stringify(vuex.state));
+        }), {
+            // Use deep so we can track changes within nested objects/arrays.
             deep: true
         });
     };
